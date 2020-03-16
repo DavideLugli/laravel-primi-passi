da480b67e766af17ab5852207ddda51816985e14
Davide Lugli <luglidavide.web@gmail.com>
2020-03-16 23:17:23 +0100

    fine

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,14 @@
+@include('layout.partials._header')
+
+<div class="container">
+<header>
+  <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
+</header>
+
+  <div class="cds-container">
+      @yield('cd-wrapper')
+  </div>
+</div>
+</body>
+
+</html>
